890f4d960f8d90d10bd6087aeefbd57335476f0e
utpalbarua <utpal.cs.07@gmail.com>
2026-01-06 18:48:00 +0600

    2nd commit data ingestion

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -6,3 +6,13 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.joins('artifacts', 'train.csv')
+    test_data_path:str = os.path.joins('artifacts', 'test.csv')
+    raw_data_path:str = os.path.joins('artifacts', 'raw.csv')
+    
+
+
