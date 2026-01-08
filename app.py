88fa40a112f611717ecc467b283c04b6dd4ba7cf
utpalbarua <utpal.cs.07@gmail.com>
2026-01-09 00:36:22 +0600

    pipeline v2

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from src.mlproject.exception import CustomException
 import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformation, DataTransformationConfig
+
 
 if __name__ == "__main__":
     logging.info("The execution has started")
@@ -10,7 +12,16 @@ if __name__ == "__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_transformation_config = DataTransformationConfig()
+        data_transformation = DataTransformation()
+
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+
+
+
 
 
     except Exception as e:
