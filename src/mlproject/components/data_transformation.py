60739e6b57e1c13759d6a1b2c52f15c3bbeb10db
utpalbarua <utpal.cs.07@gmail.com>
2026-01-08 20:47:47 +0600

    data transsform v3

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -61,3 +61,34 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys)
         
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+
+
+            # divide train dataset into X, y
+            input_features_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            # divide test dataset into X, y
+            input_features_test_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = train_df[target_column_name]
+
+            logging.info("Applying Preprocessing on training and test dataset")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_features_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_features_test_df)
+
+
+
+
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
