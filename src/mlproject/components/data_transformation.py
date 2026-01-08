a25eefa55cdb862eae9b58e8ca4fa69f033df295
utpalbarua <utpal.cs.07@gmail.com>
2026-01-08 20:32:02 +0600

    data transformation v2

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -16,3 +16,48 @@ import os
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        this function is responsible for data transformation
+        '''
+        
+        try:
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+            "gender",
+            "race_ethnicity",
+            "parental_level_of_education",
+            "lunch",
+            "test_preparation_course",  
+            ]    
+
+            num_pipeline = Pipeline(steps = [
+                ("imputer", SimpleImputer(strategy='median')),
+                ("scaler", StandardScaler())
+            ])
+
+            cat_pipeline = Pipeline(steps=[
+                ("imputer", SimpleImputer(strategy='most_frequent')),
+                ("one_hot_encoder", OneHotEncoder()),
+                ("scaler", StandardScaler(with_mean=False))
+            ])
+
+            logging.info(f"Categorical Columns: {categorical_columns}")
+            logging.info(f"Numerical Columns: {numerical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns)
+                    ("cat_pipeline", cat_pipeline, categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
